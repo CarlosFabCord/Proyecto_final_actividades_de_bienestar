9b9a0662de804ebe4308cee55bad3c4a107364c6
Johann0811 <johann_edu.gonzalez@uao.edu.co>
2025-05-27 03:46:21 -0500

    Se añadió una clase en la cual estan los graficos solicitados

--- Analisis.py
+++ Analisis.py
@@ -3,11 +3,24 @@ import os
 import tkinter as tk
 from tkinter import messagebox
 import matplotlib.pyplot as plt
+import seaborn as sns
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from PIL import Image
 
+sns.set_theme()
+
+def pasar_a_pil(grafico):
+  buffer = io.BytesIO() 
+  canvas = FigureCanvasAgg(grafico) 
+  canvas.draw()
+  canvas.print_png(buffer)
+  buffer.seek(0)
+  return Image.open(buffer) 
 base_personas = "base_de_usuarios.csv"
 base_facturacion="base_de_facturaciones.csv"
 
-class UsuarioAnalyzer():
+class usuario_analisis():
    def __init__(self, df):
         self.df = df
 
@@ -16,7 +29,7 @@ class UsuarioAnalyzer():
         buscar=usuario_registro.get()
         archivo_existe = os.path.isfile(base_personas)
         if archivo_existe==True:
-           data = pd.read_csv(base_personas) #en mi caso lo convertiría en diccionario
+           data = pd.read_csv(base_personas)
            data["No. identidad"] = data["No. identidad"].astype(str)
            uni_fila=data[data['No. identidad']==buscar]
            lista=uni_fila.values.tolist()[0]
@@ -45,7 +58,7 @@ def editar_datos(self):
       data.to_csv(base_personas, index=False)
 
     else:
-      arc_inexistente="El archivo no se ha creado aún. Registre al menos un proyecto"
+      arc_inexistente="El archivo no se ha creado aún. Registre al menos un usuario"
       resultado.set(arc_inexistente)
 
   except IndexError:
@@ -76,3 +89,33 @@ def elim_usuario(self):
 
   except IndexError:
         messagebox.showerror("Error", "El usuario no existe")
+
+
+class graficos(usuario_analisis):
+  def _init_(self, data_frame):
+    self.df= data_frame
+
+  def graficar_barras(self):
+    df = pd.read_csv(base_facturacion)
+    db = pd.read_csv(base_personas)
+    edad = db["Edad"]
+    cantidad_clases = df['Clases Spinning','Clases Fisioterapia','Clases Rumba','Clases Fortalecimiento']
+    clases = ['Spinning','Fisioterapia','Rumba','Fortalecimiento']
+    grafico, ax = plt.subplots(1, 3, figsize=(9,3), dpi=100) 
+    sns.barplot(data= self.df, x= clases , y= cantidad_clases, ax=ax[0])
+    ax[0].set_title("Grafico de usuarios por clase", fontsize=7)
+    ax[0].set_xlabel(clases, fontsize=6)
+    ax[0].set_ylabel(cantidad_clases, fontsize=6)
+    ax[0].tick_params(axis='x', labelsize=6)
+
+    sns.histplot(edad, bins = 10, kde = True, ax =ax[1])
+    ax[1].title("Histograma de edades")
+    ax[1].set_xlabel("Edades", fontsize=7)
+    ax[1].set_ylabel("Frecuencia", fontsize=7)
+
+    colores = ["red","green", "blue"]
+    ax[2].pie(cantidad_clases, labels=cantidad_clases.index, colors=colores, shadow=True, textprops={'fontsize':6})
+    ax[2].set_title("Inscripciones por actividad")
+    grafico.tight_layout()   
+    return pasar_a_pil(grafico)
+  
