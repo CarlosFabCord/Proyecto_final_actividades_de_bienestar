9c6e6b674793168943b78c943c3994fdff9e6ebb
Johann0811 <johann_edu.gonzalez@uao.edu.co>
2025-05-28 00:52:58 -0500

    Se agreagaron las funciones a una nueva clase llamada Analisis

--- Analisis.py
+++ Analisis.py
@@ -4,102 +4,128 @@ import tkinter as tk
 from tkinter import messagebox
 import matplotlib.pyplot as plt
 import seaborn as sns
-import io
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from PIL import Image
 
-sns.set_theme()
 
-def pasar_a_pil(grafico):
-  buffer = io.BytesIO() 
-  canvas = FigureCanvasAgg(grafico) 
-  canvas.draw()
-  canvas.print_png(buffer)
-  buffer.seek(0)
-  return Image.open(buffer) 
-base_personas = "base_de_usuarios.csv"
-base_facturacion="base_de_facturaciones.csv"
+                    ################################ TRABAJAR CON REGISTROS #################################################
 
-class usuario_analisis():
-   def __init__(self, df):
-        self.df = df
+class usuario():
+   def __init__(self, buscar, base_personas, base_facturacion, nombre_de_buscado, edad_de_buscado):
+        
+        self.buscar=buscar
+        self.base_personas=base_personas
+        self.base_facturacion=base_facturacion
+        self.nombre_de_buscado=nombre_de_buscado
+        self.edad_de_buscado=edad_de_buscado
+        
+
+####################################################################################################
 
    def mostrar_registro(self):
-    try:
-        buscar=usuario_registro.get()
-        archivo_existe = os.path.isfile(base_personas)
+
+    try:        
+        archivo_existe = os.path.isfile(self.base_personas)
         if archivo_existe==True:
-           data = pd.read_csv(base_personas)
+           data = pd.read_csv(self.base_personas) #en mi caso lo convertiría en diccionario
            data["No. identidad"] = data["No. identidad"].astype(str)
-           uni_fila=data[data['No. identidad']==buscar]
+           uni_fila=data[data['No. identidad']==self.buscar]
            lista=uni_fila.values.tolist()[0]
-           nombre.set(lista[1])
-           edad.set(lista[2])
-           meses.set(lista[3])
+           return lista
+        
         else:
            arc_inexistente="El archivo no se ha creado aún. Registre al menos un usuario"
-           resultado.set(arc_inexistente)
+           
 
     except IndexError:
-      messagebox.showerror("Error", "El usuario no existe")
+      messagebox.showerror("Error", "El usuario no existe")###
+
+###############################################################################################
+
+
+#def __init__(self, buscar, base_personas, base_facturacion, nombre_de_buscado, edad_de_buscado)
+        
+        #self.buscar=buscar
+        #self.base_personas=base_personas
+        #self.base_facturacion=base_facturacion
+        #self.nombre_de_buscado=nombre_de_buscado
+        #self.edad_de_buscado=edad_de_buscado
 
 
 def editar_datos(self):
 
-  try:
-    buscar=usuario_registro.get()
-    archivo_existe = os.path.isfile(base_personas)
-    if archivo_existe==True:
-      data = pd.read_csv(base_personas)
-      data["No. identidad"] = data["No. identidad"].astype(str)
-      indice = data['No. identidad']==buscar
-      data.loc[indice, 'Nombre'] = nombre.get()
-      data.loc[indice, 'Edad'] = edad.get()
-      data.to_csv(base_personas, index=False)
+        try:        
+            archivo_existe = os.path.isfile(self.base_personas)
+            
+            if archivo_existe==True:
+            
+                data = pd.read_csv(self.base_personas)
 
-    else:
-      arc_inexistente="El archivo no se ha creado aún. Registre al menos un usuario"
-      resultado.set(arc_inexistente)
+                data["No. identidad"] = data["No. identidad"].astype(str)
 
-  except IndexError:
-        messagebox.showerror("Error", "El proyecto no existe")
+                indice = data['No. identidad']==self.buscar
+
+                data.loc[indice, 'Nombre'] = self.nombre_de_buscado
+                data.loc[indice, 'Edad'] = self.edad_de_buscado
+
+                data.to_csv(self.base_personas, index=False)
+
+            else:
+                arc_inexistente="El archivo no se ha creado aún. Registre al menos un usuario"
+            
+
+        except IndexError:
+                messagebox.showerror("Error", "El usuario no existe")
+
+#################################################################################################
+
+#----Referencia de ingredientes necesarios de la clase:-----
+
+#def init(self, buscar, base_personas, base_facturacion, nombre_de_buscado, edad_de_buscado)
+        
+        #self.buscar=buscar
+        #self.base_personas=base_personas
+        #self.base_facturacion=base_facturacion
+        #self.nombre_de_buscado=nombre_de_buscado
+        #self.edad_de_buscado=edad_de_buscado
 
 
 def elim_usuario(self):
+        
+        try:
 
-  try:
-    buscar=usuario_registro.get()
-    archivo_existe = os.path.isfile(base_personas), os.path.isfile(base_facturacion)
+            archivo_existe = os.path.isfile(self.base_personas) and os.path.isfile(self.base_facturacion)
 
-    if archivo_existe==True:
-      data = pd.read_csv(base_personas)
-      data_fac = pd.read_csv(base_facturacion)
-      data["No. identidad"] = data["No. identidad"].astype(str)
-      data_fac["No. identidad"] = data_fac["No. identidad"].astype(str)
-      indice = data[data['No. identidad']==buscar].index
-      indice_fac = data_fac[data_fac['No. identidad']==buscar].index
-      data.drop(index=indice, inplace=True)
-      data.drop(index=indice_fac, inplace=True)
-      data.to_csv(base_personas, index=False)
-      data.to_csv(base_facturacion, index=False)
+            if archivo_existe==True:
+            
+                data = pd.read_csv(self.base_personas)
+                data_fac = pd.read_csv(self.base_facturacion)
 
-    else:
-      arc_inexistente="El archivo no se ha creado aún. Registre al menos un usuario"
-      resultado.set(arc_inexistente)
+                data["No. identidad"] = data["No. identidad"].astype(str)
+                data_fac["No. identidad"] = data_fac["No. identidad"].astype(str)
 
-  except IndexError:
-        messagebox.showerror("Error", "El usuario no existe")
+                indice = data[data['No. identidad']==self.buscar].index
+                indice_fac = data_fac[data_fac['No. identidad']==self.buscar].index
 
+                data.drop(index=indice, inplace=True)
+                data.drop(index=indice_fac, inplace=True)
+                data.to_csv(self.base_personas, index=False)
+                data.to_csv(self.base_facturacion, index=False)
 
-class graficos(usuario_analisis):
-  def _init_(self, data_frame):
-    self.df= data_frame
+            else:
+                arc_inexistente="El archivo no se ha creado aún. Registre al menos un usuario"
+            
+
+        except IndexError:
+                messagebox.showerror("Error", "El usuario no existe")
+
+class Analisis():
+  def __init__(self,base_personas,base_facturacion):
+    self.base_personas=base_personas
+    self.base_facturacion=base_facturacion
+
 
   def graficar_barras(self):
-    df = pd.read_csv(base_facturacion)
-    db = pd.read_csv(base_personas)
-    edad = db["Edad"]
-    cantidad_clases = df['Clases Spinning','Clases Fisioterapia','Clases Rumba','Clases Fortalecimiento']
+
+    cantidad_clases = self.base_facturacion['Clases Spinning','Clases Fisioterapia','Clases Rumba','Clases Fortalecimiento']
     clases = ['Spinning','Fisioterapia','Rumba','Fortalecimiento']
     grafico, ax = plt.subplots(1, 3, figsize=(9,3), dpi=100) 
     sns.barplot(data= self.df, x= clases , y= cantidad_clases, ax=ax[0])
@@ -108,14 +134,31 @@ class graficos(usuario_analisis):
     ax[0].set_ylabel(cantidad_clases, fontsize=6)
     ax[0].tick_params(axis='x', labelsize=6)
 
-    sns.histplot(edad, bins = 10, kde = True, ax =ax[1])
-    ax[1].title("Histograma de edades")
-    ax[1].set_xlabel("Edades", fontsize=7)
-    ax[1].set_ylabel("Frecuencia", fontsize=7)
-
     colores = ["red","green", "blue"]
-    ax[2].pie(cantidad_clases, labels=cantidad_clases.index, colors=colores, shadow=True, textprops={'fontsize':6})
-    ax[2].set_title("Inscripciones por actividad")
+    ax[1].pie(cantidad_clases, labels=cantidad_clases.index, colors=colores, shadow=True, textprops={'fontsize':6})
+    ax[1].set_title("Inscripciones por actividad")
     grafico.tight_layout()   
     return pasar_a_pil(grafico)
   
+
+  def total_usuarios(self):
+    cantidad_total = self.df['No. identidad'].sum()
+    return cantidad_total
+    
+  def incompletos(self):                                             
+    columnas_verificar = ["No. identidad", "Nombre", "Edad", "Meses"]
+  
+    filas_nulos = self.df[self.df[columnas_verificar].isnull().any(axis=1)]
+
+    cantidad_filas_nulas = len(filas_nulos)
+
+    return cantidad_filas_nulas
+    
+  def promedio_pagos(self):
+    pagos = self.df["Valor_acomulado"].sum()
+    promedio = pagos/len(self.df["Valor_acomulado"])
+    return promedio
+    
+  def actividad_mas_demandada(self):
+    actividad = max(self.df["Actividad"].value_counts().tolist())
+    return actividad
